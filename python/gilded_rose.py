71d3ea469444ebbf0e2a4261555b4d377b385ace
Erick Hualla <ehuallap@unsa.edu.pe>
2022-12-13 06:20:09 -0500

    Final refactoring done

--- python/gilded_rose.py
+++ python/gilded_rose.py
@@ -1,40 +1,60 @@
 # -*- coding: utf-8 -*-
+backstage = "Backstage passes to a TAFKAL80ETC concert"
+a_brie = "Aged Brie"
+sulfuras = "Sulfuras, Hand of Ragnaros"
 
 class GildedRose(object):
-
     def __init__(self, items):
         self.items = items
-
+    def update_backstage(self, item):
+        if item.sell_in <= 0:
+            item.quality = 0
+        elif item.sell_in <= 5:
+            item.quality += 3
+        elif item.sell_in <= 10:
+            item.quality += 2
+        else:
+            item.quality += 1
+        item.sell_in -= 1
+        item.quality = min(item.quality, 50)
+        
+    def update_brie(self, item):
+        if item.sell_in <= 0:
+            item.quality += 2
+        else:
+            item.quality += 1
+        item.sell_in -= 1
+        item.quality = min(item.quality, 50)
+            
+        
+    def update_other(self, item):
+        if item.sell_in <= 0:
+            item.quality -= 2
+        else:
+            item.quality -= 1
+        item.sell_in -= 1
+        item.quality = max(item.quality, 0)
+        item.quality = min(item.quality, 50)
+    
+    def update_conjured(self, item):
+        if item.sell_in <= 0:
+            item.quality -= 4
+        else:
+            item.quality -= 2
+        item.sell_in -= 1
+        item.quality = max(item.quality, 0)
+        item.quality = min(item.quality, 50)
+    
     def update_quality(self):
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
-
+            if item.name[0:8] == "Conjured":
+                self.update_conjured(item)
+            elif item.name == backstage:
+                self.update_backstage(item)
+            elif item.name == a_brie:
+                self.update_brie(item)
+            elif item.name != sulfuras:
+                self.update_other(item)
 
 class Item:
     def __init__(self, name, sell_in, quality):
--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -3,13 +3,48 @@ import unittest
 
 from gilded_rose import Item, GildedRose
 
+backstage = "Backstage passes to a TAFKAL80ETC concert"
+a_brie = "Aged Brie"
+sulfuras = "Sulfuras, Hand of Ragnaros"
 
 class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
+    def test_existentes(self):
+        items = [
+            Item("Zanahoria", 10, 5),   #decremento de 1 unidad
+            Item("Tomate", 0, 4),       #decremento de 2 unidades
+            Item("Cebolla", 2, 0),      #calidad no será negativa
+            Item(a_brie, 4, 50),   #calidad no será mayor de 50
+            Item(a_brie, 0, 40),   #calidad aumentará en 2 unidades
+            Item(sulfuras, 50, 80), #ni calidad ni fecha cambiará
+            Item(backstage, 15, 20), #calidad aumentará en 1 unidad
+            Item(backstage, 10, 20), #calidad aumentará en 2 unidades
+            Item(backstage, 5, 20),  #calidad aumentará en 3 unidades
+            Item(backstage, 0, 20),  #calidad caerá a 0
+            ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        self.assertEquals("fixme", items[0].name)
+        self.assertEqual("Zanahoria, 9, 4", items[0].__repr__())
+        self.assertEqual("Tomate, -1, 2", items[1].__repr__())
+        self.assertEqual("Cebolla, 1, 0", items[2].__repr__())
+        self.assertEqual(a_brie + ", 3, 50", items[3].__repr__())
+        self.assertEqual(a_brie + ", -1, 42", items[4].__repr__())
+        self.assertEqual(sulfuras + ", 50, 80", items[5].__repr__())
+        self.assertEqual(backstage + ", 14, 21", items[6].__repr__())
+        self.assertEqual(backstage + ", 9, 22", items[7].__repr__())
+        self.assertEqual(backstage + ", 4, 23", items[8].__repr__())
+        self.assertEqual(backstage + ", -1, 0", items[9].__repr__())
+    
+    def test_nuevo(self):
+        items = [
+            Item("Conjured Mana Cake", 10, 5),   #decremento de 2 unidades
+            Item("Conjured Mana Apple", 0, 4),    #decremento de 4 unidades
+            Item("Conjured Mana Orange", 2, 0),   #calidad no será negativa
+            ]
+        gilded_rose = GildedRose(items)
+        gilded_rose.update_quality()
+        self.assertEqual("Conjured Mana Cake, 9, 3", items[0].__repr__())
+        self.assertEqual("Conjured Mana Apple, -1, 0", items[1].__repr__())
+        self.assertEqual("Conjured Mana Orange, 1, 0", items[2].__repr__())
 
         
 if __name__ == '__main__':
